54fde9f222c63cd046e13fd93ff5db7a744a923b
Kaushalgothadiya <87066653+Kaushalgothadiya@users.noreply.github.com>
2024-03-29 14:10:36 +0000

    varriable

--- 01_basics/varriables.js
+++ 01_basics/varriables.js
@@ -1,1 +1,17 @@
-console.log('hello');
+const password="kaushal"
+let my_name="Gothadiya Kaushal"
+var account="SBI"
+country="India"
+
+// we can not changed const:
+// password="newpassword"
+
+// we can change the let:
+// my_name="new name"
+
+// we can not redeclare the my_name:
+// let my_name="new name"
+
+
+
+console.table([password,my_name,account,country])
